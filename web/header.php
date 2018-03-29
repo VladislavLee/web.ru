89cd275943aa06ca980a975b116e82a122853e7c
nikitaDyadechkin <nikitadyadechkin32115@gmail.com>
2018-03-29 22:16:50 +0400

    remove photo on header

--- web/header.php
+++ web/header.php
@@ -87,17 +87,4 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     </div>
     <div class="clearfix"> </div>
 </div>
-<!---->
-<div class="banner">
-    <div class="container">
-        <div class="banner-matter">
-            <h1>Get active get running push your limits</h1>
-            <div class="out">
-                <a href="single.php" class="find">FIND OUT MORE </a>
-                <a href="single.php" class="shop">SHOP</a>
-                <div class="clearfix"> </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!---->
+
